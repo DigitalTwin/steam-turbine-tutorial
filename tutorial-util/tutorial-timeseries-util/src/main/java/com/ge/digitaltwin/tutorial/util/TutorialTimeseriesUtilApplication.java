1728bb5af3ac826e9593a8422744e156f0086803
Gregg K. Steuben <steuben@ge.com>
2016-10-31 15:11:20 -0400

    Found references to deprecated predix-time-series-client, changed it to digital-twin-time-series-client

--- tutorial-util/tutorial-timeseries-util/src/main/java/com/ge/digitaltwin/tutorial/util/TutorialTimeseriesUtilApplication.java
+++ tutorial-util/tutorial-timeseries-util/src/main/java/com/ge/digitaltwin/tutorial/util/TutorialTimeseriesUtilApplication.java
@@ -1,12 +1,12 @@
 package com.ge.digitaltwin.tutorial.util;
 
-import com.ge.dt.ptsc.PredixTimeSeriesClientConfiguration;
+import com.ge.dt.tsc.DigitalTwinTimeSeriesClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(PredixTimeSeriesClientConfiguration.class)
+@Import(DigitalTwinTimeSeriesClientConfiguration.class)
 public class TutorialTimeseriesUtilApplication {
     public static void main(String args[]) {
         SpringApplication.run(TutorialTimeseriesUtilApplication.class, args);
